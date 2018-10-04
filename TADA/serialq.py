7cb594449befe71b433cad04e968ac852df87ebe
Mark <redddogjr@gmail.com>
2018-10-04 14:23:23 -0600

    refactor Tada

---
+++ TADA/serialq.py
@@ -0,0 +1,21 @@
+
+
+# Serial(
+# self.serial_port(), 
+# self.baud_rate,
+# timeout=1.0)
+
+class Serial():
+    def __init__(self, port, baud_rate, timeout=0):
+        pass
+    
+    def isOpen():
+        return True
+    
+    
+    
+    def open():
+        pass
+    
+    def close():
+        pass
