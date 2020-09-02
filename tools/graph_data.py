b78d00a0af541877c0b1dc3473689b72952abf29
Mark Redd <redddogjr@gmail.com>
2020-09-02 16:14:37 -0600

    fixed some tools

--- tools/graph_data.py
+++ tools/graph_data.py
@@ -8,59 +8,76 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def plot_data(data_path):
-    filename = data_path.split('/')[-1]
+def get_dataframe(data_path):
+
+    
     with open(data_path, 'r') as f:
         f.readline()
         run_info = f.readline()
         headertxt = "time,t1,t2,t3,t4"
-        for i, line in enumerate(f.readlines()):
-            header = i + 1
-            if headertxt in line: break
 
-    # with open(data_path, 'r') as f:
+    header_row = 0
+    with open(data_path, 'r') as f:
+        for i, line in enumerate(f.readlines()):
+            if "time,t1,t2,t3,t4,pressure" in line:
+                header_row = i
+                break
+
     try:
-        print(header)
-        data = pd.read_csv(data_path, header=header)
+        data = pd.read_csv(
+            data_path, 
+            header=header_row,
+            index_col=False, 
+            usecols=["time","t1","t2","t3","t4","pressure"]
+        )
+        return data, run_info
     except Exception as e:
         print(f"File: {data_path}: {type(e)}: {e}")
-        return
+        return None, None
+
+def plot_data(data_path):
+    filename = data_path.split('/')[-1] 
+    data, run_info = get_dataframe(data_path)
     
-    print(data.keys())
+    assert data is not None and run_info is not None, "Data retrival failed."
+
     ignition_status = run_info.split(',')[5]
     if "N/A" in ignition_status: ignition_status = "No"
     elif ignition_status == "": ignition_status = "Unknown"
-    
+
     times = np.array(data['time'])
-    time_pts = np.array((times - times[0]) / 60)
-    internal_temp = np.array(data['t4'])
+    time_pts = (times - times[0]) / 60
     pressure = np.array(data['pressure'])
-    test_temp = find_test_temp(time_pts, internal_temp)
+    test_temp, min_ = find_test_temp(time_pts, data)
+    pm = test_temp - min_
     data_title = filename[:-4] + \
-        " (Temp: {0:.2f} \u00B0C - {1} ignition)".format(
-        test_temp, ignition_status)
-    
+        f" (Temp: {test_temp:.2f} +/- {pm:.2f} \u00B0C - {ignition_status} ignition)"
+
     plt.clf()
     plt.subplot(211)
     plt.title(data_title)
-    plt.plot(time_pts, internal_temp)
+
+    for label in data.keys():
+        if "Unnamed" in label or\
+            "time" in label or\
+            "pressure" in label:
+            continue
+        
+        temps = np.array(data[label])
+        plt.plot(time_pts, temps, label=label)
+
     plt.ylabel('Temperature (\u00B0C)')
+    plt.legend()
     plt.subplot(212)
     plt.plot(time_pts, pressure, 'r')
     plt.xlabel('time (min)')
     plt.ylabel('Pressure (torr)')
 
 
-def find_test_temp(time_data, temp_data):
-    
-    # for i in range(len(time_data)):
-    #     dT_dt_test =    (temp_data[i] - temp_data[i+3])/\
-    #                     (time_data[i] - time_data[i+3]) * 1000
-    #     if dT_dt_test < -0.5 and i > 10:
-    #         return np.mean(temp_data[i-10:i])
-    return np.mean(temp_data[:10])
+def find_test_temp(time_data, data):
+    return np.mean(data['t4'][:10]), min([np.mean(data[f't{i+1}'][:10]) for i in range(4)])
 
- 
+
 def recurse_plot(path):
     for root, dirs, filenames in os.walk(path):
         for filename in filenames:
@@ -102,11 +119,12 @@ def main():
         filename = tk.filedialog.askopenfilename(parent=root)
         if not filename: return
         root.destroy()
-        try:
-            plot_data(filename)
-        except Exception as e:
-            print(f"File: {filename}: {type(e)}: {e}")
-            return
+        plot_data(filename)
+        # try:
+        #     plot_data(filename)
+        # except Exception as e:
+        #     print(f"File: {filename}: {type(e)}: {e}")
+        #     return
         plt.show()
     elif argv[1] == '-r':
         dirname = tk.filedialog.askdirectory(parent=root)
